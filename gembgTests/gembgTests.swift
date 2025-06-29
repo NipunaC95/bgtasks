b7c8c03b2aa7a9d7967ca3376bbe9a0918cbb617
Nipuna <chandimal.nipuna.1995@gmail.com>
2025-06-29 14:15:02 +0530

    Initial Commit

---
+++ gembgTests/gembgTests.swift
@@ -0,0 +1,17 @@
+//
+//  gembgTests.swift
+//  gembgTests
+//
+//  Created by Nipuna Chandimal on 2025-06-29.
+//
+
+import Testing
+@testable import gembg
+
+struct gembgTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
